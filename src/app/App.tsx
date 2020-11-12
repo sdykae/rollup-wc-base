4065dfd345cecc8fe19cfdbf2b6f0001c2b858ce
sdyalor <sdyalor@outlook.com>
2020-11-12 07:50:08 -0500

    settings

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,35 @@
-import React, {FC} from 'react'
-import AppStyles from './app.scss';
-import {unsafeCSS} from 'lit-element';
+import React, { FC } from "react";
+import AppStyles from "./app.scss";
+import { unsafeCSS } from "lit-element";
+
+import {
+  createPrinter,
+  Printer,
+  SourceFile,
+  createSourceFile,
+  ScriptTarget,
+  ScriptKind,
+} from "typescript";
+
+const printer: Printer = createPrinter();
+const sourceFile: SourceFile = createSourceFile(
+  "test.ts",
+  "const x  :  number = 42",
+  ScriptTarget.ES2015,
+  true,
+  ScriptKind.TS
+);
+console.log(printer.printFile(sourceFile));
+
 const App = () => {
-  
+  //@ts-ingore
+  //@ts-nocheck
+  document.adoptedStyleSheets = [
+    ...document.adoptedStyleSheets,
+    unsafeCSS(AppStyles).styleSheet,
+  ];
 
-  document.adoptedStyleSheets = [...document.adoptedStyleSheets, unsafeCSS(AppStyles).styleSheet];
+  return <div>My component 🧚‍♀️✨</div>;
+};
 
-  return <div>My component 🧚‍♀️✨</div>
-}
-
-export default App
+export default App;
